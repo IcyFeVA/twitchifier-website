a2d2930a5e086f7f3fd15a181df102028fb53240
Pascal Zirn <zirn.pascal@gmail.com>
2021-11-15 22:47:04 +0100

    better text

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,8 +11,8 @@ const AboutPage = () => (
     <h1 style={{marginTop: `64px`}}>Why was Twitchifier born?</h1>
     <p style={{fontSize: `1.1rem`, marginBottom: `64px`, lineHeight: `170%`}}>
       Hello there!<br /><br />
-      My name is Pascal. I created this app, because I was looking for a way to know when my favorite streamers go online, without having to constantly check the website, or install the offical Twitch app.<br />
-      I'm following a ton of streamers, but I don't necessarily need to get notified, anytime one of them goes live. Otherwise I would get flooded by notifications. So, if you're weird like me, you might want to buy this app :)
+      My name is Pascal. I created this app, because I was looking for a way to know when my favorite streamers go online, without having to constantly check the website, or install the offical Twitch app.
+I'm following a ton of streamers, but I don't necessarily need to get notified, anytime one of them goes live. Only certain streamers deserve to be on my list and interrupt my flow :)
     </p>
   </Layout>
 )
