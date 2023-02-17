c14bbda08efc70c020142dcef25e6dfb644b8e5a
IcyFeVA <zirn.pascal@gmail.com>
2023-02-16 23:17:39 -0800

    new donation text

--- src/pages/download.js
+++ src/pages/download.js
@@ -15,7 +15,7 @@ const AboutPage = () => (
         Download
       </button>
     </a>
-    <p className="info" style={{marginTop: `64px`}}>As of February 2023 my job will be redundant and me, my wife and kids appreciate any donation. Thank you.</p>
+    <p className="info" style={{marginTop: `64px`}}>Donate if you like. Thank you.</p>
     <a href="https://www.paypal.com/donate/?hosted_button_id=UUNUPVB2ES8FS" target="_blank">
       <button className="checkout" role="link">
         Donate Now
